e918a8d9d424a2e88a5fb29ca99c746c47a66856
Alex Bernal <csrbernal609@gmail.com>
2018-09-16 02:39:14 -0500

    fix install singularity

--- setup.sh
+++ setup.sh
@@ -27,8 +27,11 @@ echo "sregistry Version:"
 # Install Singularity
 
 cd /tmp && \
-    git clone https://github.com/singularityware/singularity.git && \
-    cd singularity && \
-    ./autogen.sh && \
-    ./configure --prefix=/usr/local && \
-    make && sudo make install
+   git clone https://github.com/singularityware/singularity.git && \
+cd singularity && \
+git fetch --all && \
+git checkout 2.6.0 && \
+./autogen.sh && \
+./configure --prefix=/usr/local $$ \
+make && \
+sudo make install
